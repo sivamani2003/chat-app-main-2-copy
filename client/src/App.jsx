c9858dd29f6a13a79098ca584826e880392439aa
siva <sivamani_k@srmap.edu.in>
2024-07-24 19:54:01 +0530

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -65,7 +65,7 @@ function App() {
   return (
     <Routes>
       {/* <Route path='/' element={<Home />} /> */}
-      <Route path='/' element={<Home />} />
+      <Route path='/home' element={<Home />} />
       <Route path='/mentorship' element={<Menotor_home1 />} />
       <Route path='/careers' element={<ResultsComponent/>} />
       <Route path="/mentorservices" element={<Services1/>}></Route>
@@ -121,7 +121,7 @@ function App() {
         <Route path="/forgot-password" element={<AuthLayouts><Forgotpassword /></AuthLayouts>} />
   
         <Route element={<ProtectedRoutes />}>
-        <Route path="/" element={<Chathome />}>
+        <Route path="/chat-home" element={<Chathome />}>
           <Route path=":userId" element={<MessagePage />} />
         </Route>
         </Route>
